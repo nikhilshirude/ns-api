8187b3dac51b309c4d8d4277f8bd653e8a959f39
nikhilshirude <nikhil.shirude@gmail.com>
2017-05-08 23:54:27 -0400

    Test file commit

--- src/main/java/TestCollections.java
+++ src/main/java/TestCollections.java
@@ -2,11 +2,7 @@
  * Created by nikhilshirude on 1/13/17.
  */
 public class TestCollections {
-
         public static void main(String[] args) {
-
             System.out.println("Hello Nikhil");
-
         }
-
 }
